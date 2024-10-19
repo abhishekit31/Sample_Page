96cf8e7872806b602a0f9bac0a32589448de98ea
Abhishek Kumar <abhishekit31@gmail.com>
2024-10-19 13:09:52 +0530

    Done some changes inside the app.js file to validate phone number and email id

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ function validateForm()
     const form = document.getElementById("form");
 
     let isValid = true;
+    const errorStyle = "color: blueviolet; font-weight: bold;"
 
     if (fname === "") {
+        document.getElementById("fname-error").innerHTML = "<span style='" + errorStyle + "'>First name is required</span>";
         document.getElementById("fname-error").style.visibility = "visible";
         isValid = false;
     } else {
@@ -16,23 +18,44 @@ function validateForm()
     }
 
     if (lname === "") {
+        document.getElementById("lname-error").innerHTML = "<span style ='" + errorStyle + "'>Last name is required</span>"
         document.getElementById("lname-error").style.visibility = "visible";
         isValid = false;
     } else {
         document.getElementById("lname-error").style.visibility = "hidden";
     }
 
+    const phonePattern = /^[0-9]{10}$/;
     if (phone === "") {
+        document.getElementById("phone-error").innerHTML = "<span style = '"+ errorStyle +"'> Phone number is required </span>"
+        document.getElementById("phone-error").style.visibility = "visible";
+        isValid = false;
+    } else if (!/^\d+$/.test(phone)) {
+        // If the input contains non-numeric characters
+        document.getElementById("phone-error").innerHTML = "<span style = '"+ errorStyle +"'>Please enter the number only</span>"
+        document.getElementById("phone-error").style.visibility = "visible";
+        isValid = false;
+    } else if (!phonePattern.test(phone)) {
+        // If the phone number is not exactly 10 digits
+        document.getElementById("phone-error").innerHTML = "<span style = '"+ errorStyle +"'>Phone number must be exactly 10 digits</span>"
         document.getElementById("phone-error").style.visibility = "visible";
         isValid = false;
     } else {
         document.getElementById("phone-error").style.visibility = "hidden";
     }
 
-    if (email === "") {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (email === "" ){
+        document.getElementById("email-error").innerHTML = "<span style = '"+ errorStyle +"'>Email Id is required</span>"
         document.getElementById("email-error").style.visibility = "visible";
         isValid = false;
-    } else {
+    }
+    else if(!emailPattern.test(email)){
+        document.getElementById("email-error").innerHTML = "<span style = '"+ errorStyle +"'>Invalid email Id it should contain @ </span>"
+        document.getElementById("email-error").style.visibility = "visible";
+        isValid = false;
+    }
+     else {
         document.getElementById("email-error").style.visibility = "hidden";
     }
 
